def890662e2b96ce0b1cfa5e9230deaf2118b41a
Veli <veli.tasali@gmail.com>
2016-10-18 21:27:14 +0300

    Kernel updater added

--- app/src/main/java/Fragment/MainFragment.java
+++ app/src/main/java/Fragment/MainFragment.java
@@ -3,16 +3,18 @@ package Fragment;
 /**
  * Created by sumit on 7/7/16.
  */
-import android.app.Fragment;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.delos.sumit.arubadel.R;
-public class MainFragment extends Fragment {
 
+public class MainFragment extends Fragment
+{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
