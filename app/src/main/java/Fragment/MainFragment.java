f60320158999f91cca498d78f80e2fa320e52c5b
Grace5921 <androidlover5842@gmail.com>
2016-10-18 23:28:50 +0530

    use rootview instead of return

--- app/src/main/java/Fragment/MainFragment.java
+++ app/src/main/java/Fragment/MainFragment.java
@@ -16,6 +16,8 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootview;
     }
 }
