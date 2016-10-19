83765cd5e901ff9bc47bc0cc546f6a49020c161a
Grace5921 <androidlover5842@gmail.com>
2016-10-19 14:16:04 +0530

    add option to turn off and on cpu1

--- app/src/main/java/Fragment/MainFragment.java
+++ app/src/main/java/Fragment/MainFragment.java
@@ -10,8 +10,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.delos.sumit.arubadel.R;
+
+import eu.chainfire.libsuperuser.Shell;
 
 public class MainFragment extends Fragment
 {
@@ -19,6 +23,28 @@ public class MainFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+        Switch cpu1 = (Switch) rootview.findViewById(R.id.cpu1);
+        //set the switch to ON
+        cpu1.setChecked(true);
+        //attach a listener to check for changes in state
+        cpu1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+@Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                            boolean isChecked)
+                {
+
+                if(isChecked)
+                    {
+                    Shell.SU.run("echo \"1\" > /sys/devices/system/cpu/cpu1/online\n");
+                    }
+                else
+                    {
+                    Shell.SU.run("echo \"0\" > /sys/devices/system/cpu/cpu1/online\n");
+                    }
+
+                }
+            });
 
         return rootview;
     }
