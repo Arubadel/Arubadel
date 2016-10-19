db60f97b31879a6a523d8dea5123dfe4065b362c
Veli <veli.tasali@gmail.com>
2016-10-19 22:41:21 +0300

    Missing classes

---
+++ app/src/main/java/com/delos/sumit/arubadel/fragment/kernelupdater/KernelReleasesBetaFragment.java
@@ -0,0 +1,18 @@
+package com.delos.sumit.arubadel.fragment.kernelupdater;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Veli on 19.10.2016.
+ */
+
+public class KernelReleasesBetaFragment extends ListFragment
+{
+
+}
