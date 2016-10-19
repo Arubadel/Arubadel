13726597c67d48f84371f99033256d1dafe7a3f1
Grace5921 <androidlover5842@gmail.com>
2016-10-19 16:21:03 +0530

    add all cpus

--- app/src/main/java/Fragment/MainFragment.java
+++ app/src/main/java/Fragment/MainFragment.java
@@ -7,6 +7,7 @@ package Fragment;
 import android.os.*;
 import android.support.annotation.*;
 import android.support.v4.app.*;
+import android.support.v7.widget.SwitchCompat;
 import android.view.*;
 import android.widget.*;
 import com.delos.sumit.arubadel.*;
@@ -15,17 +16,23 @@ import java.util.*;
 
 public class MainFragment extends Fragment
 {
-	private Switch mCPU1;
+	private SwitchCompat mCPU0;
+	private SwitchCompat mCPU1;
+	private SwitchCompat mCPU2;
+	private SwitchCompat mCPU3;
 
-    @Nullable
+	@Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
         View rootview = inflater.inflate(R.layout.fragment_main, container, false);
-        mCPU1 = (Switch) rootview.findViewById(R.id.cpu1);
-  
-        //attach a listener to check for changes in state
-        mCPU1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() 
+		mCPU0 = (SwitchCompat) rootview.findViewById(R.id.cpu0);
+		mCPU1 = (SwitchCompat) rootview.findViewById(R.id.cpu1);
+		mCPU2 = (SwitchCompat) rootview.findViewById(R.id.cpu2);
+		mCPU3 = (SwitchCompat) rootview.findViewById(R.id.cpu3);
+
+		//attach a listener to check for changes in state
+		mCPU0.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
 			{
 				@Override
 				public void onCheckedChanged(CompoundButton buttonView,
@@ -50,18 +57,57 @@ public class MainFragment extends Fragment
 	{
 		super.onResume();
 		
-		List<String> resultList = Shell.SU.run("cat /sys/devices/system/cpu/cpu1/online\n");
-		
-		if (resultList.size() > 0)
+		List<String> cpu0_resultList = Shell.SU.run("cat /sys/devices/system/cpu/cpu1/online\n");
+		List<String> cpu1_resultList = Shell.SU.run("cat /sys/devices/system/cpu/cpu2/online\n");
+		List<String> cpu2_resultList = Shell.SU.run("cat /sys/devices/system/cpu/cpu2/online\n");
+		List<String> cpu3_resultList = Shell.SU.run("cat /sys/devices/system/cpu/cpu3/online\n");
+
+		if (cpu0_resultList.size() > 0)
 		{
 			// catch if bash send wrong type of string
 			try
 			{
-				boolean currentState = 1 == Integer.valueOf(resultList.get(0));
-				this.mCPU1.setChecked(currentState);
+				boolean currentState = 1 == Integer.valueOf(cpu0_resultList.get(0));
+				this.mCPU0.setChecked(currentState);
 			}
 			catch(Exception e)
 			{}
 		}
+		if (cpu1_resultList.size() > 0)
+		{
+			// catch if bash send wrong type of string
+			try
+			{
+				boolean currentState = 1 == Integer.valueOf(cpu1_resultList.get(0));
+				this.mCPU2.setChecked(currentState);
+			}
+			catch(Exception e)
+			{}
+		}
+
+		if (cpu2_resultList.size() > 0)
+		{
+			// catch if bash send wrong type of string
+			try
+			{
+				boolean currentState = 1 == Integer.valueOf(cpu2_resultList.get(0));
+				this.mCPU2.setChecked(currentState);
+			}
+			catch(Exception e)
+			{}
+		}
+
+		if (cpu3_resultList.size() > 0)
+		{
+			// catch if bash send wrong type of string
+			try
+			{
+				boolean currentState = 1 == Integer.valueOf(cpu3_resultList.get(0));
+				this.mCPU3.setChecked(currentState);
+			}
+			catch(Exception e)
+			{}
+		}
+
 	}
 }
