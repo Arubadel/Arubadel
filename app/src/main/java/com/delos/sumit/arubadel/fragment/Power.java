cc13d078271394d6994696bcbbfc464ced51b67d
Grace5921 <androidlover5842@gmail.com>
2016-10-21 02:07:22 +0530

    add fast charge

---
+++ app/src/main/java/com/delos/sumit/arubadel/fragment/Power.java
@@ -0,0 +1,111 @@
+package com.delos.sumit.arubadel.fragment;
+
+/**
+ * Created by sumit on 7/7/16.
+ */
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.delos.sumit.arubadel.R;
+import com.delos.sumit.arubadel.app.Activity;
+import com.delos.sumit.arubadel.util.ShellUtils;
+
+import java.util.List;
+
+import eu.chainfire.libsuperuser.Shell;
+
+public class Power extends Fragment
+{
+    private SwitchCompat mMPDecision;
+    private SwitchCompat fastCharge;
+    private SwitchCompat mCPU2;
+    private SwitchCompat mCPU3;
+    private TextView Powertext;
+
+    private ShellUtils mShell;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // get terminal session
+        this.mShell = ((Activity) getActivity()).getShellSession();
+
+        View rootView = inflater.inflate(R.layout.fragment_power, container, false);
+
+        fastCharge = (SwitchCompat) rootView.findViewById(R.id.fast_charge);
+        Powertext = (TextView) rootView.findViewById(R.id.Powertext);
+
+        //attach a listener to check for changes in state
+        fastCharge.setOnCheckedChangeListener(createSwitchListener(1));
+
+        return rootView;
+    }
+
+    private void updateCpuState(final SwitchCompat switchView, final int cpuId)
+    {
+        mShell.getSession().addCommand("cat sys/kernel/fast_charge/force_fast_charge\n", cpuId, new Shell.OnCommandResultListener()
+        {
+            @Override
+            public void onCommandResult(int commandCode, int exitCode, List<String> output)
+            {
+                if (commandCode == cpuId)
+                {
+                    if (output.size() > 0)
+                    {
+                        // catch if bash send wrong type of string
+                        try
+                        {
+                            boolean currentState = 1 == Integer.valueOf(output.get(0));
+                            switchView.setChecked(currentState);
+                        } catch (Exception e)
+                        {
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        updateCpuState(this.fastCharge, 1);
+
+        mShell.getSession().addCommand("pgrep mpdecision", 10, new Shell.OnCommandResultListener()
+        {
+            @Override
+            public void onCommandResult(int commandCode, int exitCode, List<String> output)
+            {
+                if (exitCode == 10)
+                    mMPDecision.setChecked(output.size() > 0);
+            }
+        });
+    }
+
+    private CompoundButton.OnCheckedChangeListener createSwitchListener(final int cpuId)
+    {
+        return new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                mShell.getSession().addCommand("echo " + ((isChecked) ? 1 : 0) + " > sys/kernel/fast_charge/force_fast_charge\n");
+                Powertext.setText((isChecked) ? "Fast charge on " : "Fast charge off");
+                Powertext.setTextColor(Color.GREEN);
+            }
+        };
+    }
+
+}
