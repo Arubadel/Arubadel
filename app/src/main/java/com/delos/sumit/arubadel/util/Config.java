67cdfc27c86bfc3faf138af5ce254930b1b0ac89
Grace5921 <androidlover5842@gmail.com>
2016-10-24 14:53:16 +0530

    redirect URL_RELASES to I8552-CM

--- app/src/main/java/com/delos/sumit/arubadel/util/Config.java
+++ app/src/main/java/com/delos/sumit/arubadel/util/Config.java
@@ -9,7 +9,7 @@ package com.delos.sumit.arubadel.util;
 public class Config
 {
     public final static String PATH_CPUS = "/sys/devices/system/cpu";
-    public final static String URL_RELASES = "https://api.github.com/repos/Arubadel/Arubadel/releases";
+    public final static String URL_RELASES = "https://api.github.com/repos/I8552-CM/android_kernel_arubaslim/releases";
     public final static String URL_CONTRIBUTERS = "https://api.github.com/repos/Arubadel/Arubadel/contributors";
 
 }
