1570fa8488c731e8228cb2f787db4b8dc0a119e2
Grace5921 <androidlover5842@gmail.com>
2016-10-28 19:44:48 +0530

    show device vesion and etc in about device fragment

--- app/src/main/java/com/delos/sumit/arubadel/fragment/AboutDevice.java
+++ app/src/main/java/com/delos/sumit/arubadel/fragment/AboutDevice.java
@@ -1,39 +1,43 @@
 package com.delos.sumit.arubadel.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.SwitchCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.delos.sumit.arubadel.R;
-import com.delos.sumit.arubadel.adapter.CPUToolsCPUListAdapter;
-import com.delos.sumit.arubadel.app.Activity;
-import com.delos.sumit.arubadel.util.CPUInfo;
-import com.delos.sumit.arubadel.util.CPUTools;
-import com.delos.sumit.arubadel.util.ConcurrentSync;
-import com.delos.sumit.arubadel.util.Config;
-import com.delos.sumit.arubadel.util.ShellUtils;
 
 import java.util.List;
 
 import eu.chainfire.libsuperuser.Shell;
+import eu.chainfire.libsuperuser.Shell.SH;
+
 /**
  * Created by sumit on 28/10/16.
  */
 
 public class AboutDevice extends Fragment
 {
+    public TextView model_text;
+    public TextView platform_release_text;
+
+    List<String> model_sh;
+    List<String> platform_release;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.fragment_about_device, container, false);
+        model_sh= Shell.SH.run("getprop ro.product.model");
+        platform_release= Shell.SH.run("getprop ro.build.version.release");
 
+        View rootView = inflater.inflate(R.layout.fragment_about_device, container, false);
+        model_text=(TextView)rootView.findViewById(R.id.model_text);
+        platform_release_text=(TextView)rootView.findViewById(R.id.platform_release_text);
+
+        model_text.setText("Model Number :- " + model_sh);
+        platform_release_text.setText("Device Version :- " + platform_release);
         return rootView;
     }
+
 }
