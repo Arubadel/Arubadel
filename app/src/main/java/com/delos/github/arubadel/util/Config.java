3294d88fe935ab009281353d20fdfb8c3eb6341e
Grace5921 <androidlover5842@gmail.com>
2016-11-06 17:11:12 +0530

    Update app updates api link

--- app/src/main/java/com/delos/github/arubadel/util/Config.java
+++ app/src/main/java/com/delos/github/arubadel/util/Config.java
@@ -10,7 +10,7 @@ public class Config
 {
     public final static String PATH_CPUS = "/sys/devices/system/cpu";
     public final static String URL_CONTRIBUTORS = "https://api.github.com/repos/Arubadel/Arubadel/contributors";
-    public final static String URL_APP_RELEASES ="https://api.github.com/repos/Arubadel/Arubadel/releases";
+    public final static String URL_APP_RELEASES ="https://api.github.com/repos/Arubadel/AppUpdates/releases";
     public final static String URL_KERNEL_RELEASES ="https://api.github.com/repos/Arubadel/Kernel/releases";
     public final static String URL_RECOVERY_RELEASES ="https://api.github.com/repos/Arubadel/Recovery/releases";
     public final static String URL_ROM_RELEASES="https://api.github.com/repos/Arubadel/ROM/releases";
