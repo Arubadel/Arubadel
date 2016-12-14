85a27bf6c556ab4b5c7a17f794fa544a249179cd
grace5921 <androidlover5842@gmail.com>
2016-12-14 19:29:16 +0530

    add firebase chat

---
+++ app/src/main/java/com/delos/github/arubadel/fragment/FirebaseChat.java
@@ -0,0 +1,50 @@
+package com.delos.github.arubadel.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.delos.github.arubadel.R;
+import com.delos.github.arubadel.util.ChatMessage;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by sumit on 14/12/16.
+ */
+
+public class FirebaseChat extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_firebase_chat, container, false);
+        FloatingActionButton fab =
+                (FloatingActionButton)rootView.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)rootView.findViewById(R.id.input);
+
+                /* Read the input field and push a new instance
+                  of ChatMessage to the Firebase database */
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                /* Clear the input */
+                input.setText("");
+            }
+        });
+        return rootView;
+
+    }
+
+}
---
+++ app/src/main/java/com/delos/github/arubadel/util/ChatMessage.java
@@ -0,0 +1,50 @@
+package com.delos.github.arubadel.util;
+
+import java.util.Date;
+
+/**
+ * Created by sumit on 14/12/16.
+ */
+
+public class ChatMessage {
+
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        // Initialize to current time
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage() {
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
