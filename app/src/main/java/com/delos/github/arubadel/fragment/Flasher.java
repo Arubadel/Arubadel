1412cfb18154ee2e2e817881a49f602ba7e373a3
grace5921 <androidlover5842@gmail.com>
2016-12-26 23:18:43 +0530

    flasher: fix file filter

--- app/src/main/java/com/delos/github/arubadel/fragment/Flasher.java
+++ app/src/main/java/com/delos/github/arubadel/fragment/Flasher.java
@@ -1,12 +1,9 @@
 package com.delos.github.arubadel.fragment;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +11,13 @@ import android.widget.RadioButton;
 
 import com.delos.github.arubadel.R;
 import com.delos.github.arubadel.app.Activity;
-import com.delos.github.arubadel.util.FileUtil;
 import com.delos.github.arubadel.util.ShellUtils;
 import com.eminayar.panter.PanterDialog;
 import com.eminayar.panter.enums.Animation;
-import com.nononsenseapps.filepicker.FilePickerActivity;
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
-import java.net.URISyntaxException;
+import java.util.regex.Pattern;
 
 import eu.chainfire.libsuperuser.Shell;
 
@@ -34,8 +31,8 @@ public class Flasher extends Fragment
 {
     private ShellUtils mShell;
     private RadioButton mFlashBoot,mFlashRecovery;
-    private static final int FILE_SELECT_CODE = 0;
-    private String path,mBootPath,mRecoveryPath,StringFlashRB;
+    private static final int FILE_PICKER_REQUEST_CODE = 1;
+    private String path,mBRootPath,StringFlashRB;
     private PanterDialog dialog;
 
     @Nullable
@@ -52,7 +49,7 @@ public class Flasher extends Fragment
         mFlashBoot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mBootPath="/sdcard/flashed";
+                mBRootPath="/sdcard/flashed";
                 StringFlashRB="Boot";
                 showFileChooser();
             }
@@ -61,7 +58,7 @@ public class Flasher extends Fragment
         mFlashRecovery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mBootPath="/sdcard/flashed_recovery";
+                mBRootPath="/sdcard/flashed_recovery";
                 StringFlashRB="Recovery";
                 showFileChooser();
             }
@@ -78,54 +75,39 @@ public class Flasher extends Fragment
     }
 
     private void showFileChooser() {
-        Intent i = new Intent(getContext(), FilePickerActivity.class);
-        i.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
-        i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
-        i.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
-        i.addCategory(".img");
-        i.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
-
-        startActivityForResult(i, FILE_SELECT_CODE);
-
+        new MaterialFilePicker()
+                .withSupportFragment(this)
+                .withRequestCode(FILE_PICKER_REQUEST_CODE)
+                .withHiddenFiles(true)
+                .withFilter(Pattern.compile(".*\\.img$"))
+                .start();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case FILE_SELECT_CODE:
-                if (resultCode == RESULT_OK) {
-                    // Get the Uri of the selected file
-                    Uri uri = data.getData();
-                    Log.d("TAG", "File Uri: " + uri.toString());
-                    // Get the path
-                    try {
-                        path = FileUtil.getPath(getContext(), uri);
-                    } catch (URISyntaxException e) {
-                        e.printStackTrace();
-                    }
-                    Log.d("TAG", "File Path: " + path);
-                    dialog = new PanterDialog(getContext());
-                    dialog.setTitle("Flash "+StringFlashRB );
-                    dialog.setHeaderBackground(R.color.colorPrimaryDark)
-                            .setMessage("Are you sure you want to Flash this "+StringFlashRB+" ?"+"\n"+path)
-                            .setPositive("Flash", new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                /*use sdcard/flash until this feature is ready*/
-                                    Shell.SU.run("dd if="+path+" of="+mBootPath);
-                                    dialog.dismiss();
-
-                                }
-                            })
-                            .setNegative("Cancel")
-                            .withAnimation(Animation.POP)
-                            .show();
-
-                }
-                break;
-        }
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            path = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            dialog = new PanterDialog(getContext());
+            dialog.setTitle("Flash "+StringFlashRB );
+            dialog.setHeaderBackground(R.color.colorPrimaryDark)
+                    .setMessage("Are you sure you want to Flash this "+StringFlashRB+" ?"+"\n"+path)
+                    .setPositive("Flash", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                                /*use sdcard/flash until this feature is ready*/
+                            Shell.SU.run("dd if="+path+" of="+mBRootPath);
+                            dialog.dismiss();
+
+                        }
+                    })
+                    .setNegative("Cancel")
+                    .withAnimation(Animation.POP)
+                    .show();
+        }
     }
+
 
 
 }
